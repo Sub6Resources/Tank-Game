6a5c01436333afd104d62653ae33cef44d8e05c8
Matthew Whitaker <matthew.w3000@gmail.com>
2017-03-01 13:48:10 -0700

    Added Explosions and Bullets

---
+++ Tank Game/Explosion.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+
+namespace Tank_Game
+{
+    class Explosion
+    {
+        public static int MAX_PROJECTILES = 25;
+        private Color color;
+        private Bullet[] shrapnel = new Bullet[MAX_PROJECTILES];
+        public Explosion() { }
+        public Explosion(Vector2 location, Game1 game, int player, Texture2D whiteRectangle, Color _color)
+        {
+            color = _color;
+            Random randy = new Random();
+            for(int i=0; i<MAX_PROJECTILES; ++i)
+            {
+                shrapnel[i] = new Bullet(game, new Rectangle(new Point((int) location.X, (int) location.Y), new Point(randy.Next(1,20),randy.Next(1,20))), new Vector2(randy.Next(-20,20),randy.Next(-20,20)), color, player, randy.Next(-((int)MathHelper.Pi), (int) MathHelper.Pi), whiteRectangle);
+            }
+        }
+        public void Draw(SpriteBatch spriteBatch) {
+            for (int i = 0; i < MAX_PROJECTILES; ++i)
+            {
+                shrapnel[i].Draw(spriteBatch);
+            }
+        }
+        public void Update()
+        {
+            for (int i = 0; i < MAX_PROJECTILES; ++i)
+            {
+                shrapnel[i].Update();
+            }
+        }
+    }
+}
--- Tank Game/Game1.cs
+++ Tank Game/Game1.cs
@@ -11,9 +11,12 @@ namespace Tank_Game
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
-
+        Texture2D whiteRectangle;
         Tank tank1;
         Tank tank2;
+        Explosion tank1Explosion;
+        Explosion tank2Explosion;
+        Bullet[] bullets = new Bullet[2];
 
 
 
@@ -31,8 +34,10 @@ namespace Tank_Game
         /// </summary>
         protected override void Initialize()
         {
-            tank1 = new Tank(this, "GreenTank", Vector2.Zero, new Vector2(2, 2), 0, 1, 1f);
-            tank2 = new Tank(this, "RedTank", new Vector2(100, 0), new Vector2(2, 2), MathHelper.Pi, 2, 1f);
+            whiteRectangle = new Texture2D(GraphicsDevice, 1, 1);
+            whiteRectangle.SetData(new[] { Color.White });
+            tank1 = new Tank(this, "GreenTank", Vector2.Zero, new Vector2(2, 2), 0, 1, 1f, whiteRectangle, Keys.W, Keys.A, Keys.S, Keys.D);
+            tank2 = new Tank(this, "RedTank", new Vector2(100, 0), new Vector2(2, 2), MathHelper.Pi, 2, 1f, whiteRectangle, Keys.Up, Keys.Left, Keys.Down, Keys.Right);
             // TODO: Add your initialization logic here
             base.Initialize();
         }
@@ -74,7 +79,37 @@ namespace Tank_Game
 
             tank1.Update(state);
             tank2.Update(state);
-
+            if(state.IsKeyDown(Keys.Space))
+            {
+                bullets[0] = tank1.Fire();
+            }
+            if(state.IsKeyDown(Keys.RightControl))
+            {
+                bullets[1] = tank2.Fire();
+            }
+            if (state.IsKeyDown(Keys.E))
+            {
+                tank1Explosion = new Explosion(tank1.location, this, 1, whiteRectangle, Color.Green);
+            }
+            if (state.IsKeyDown(Keys.RightShift))
+            {
+                tank2Explosion = new Explosion(tank2.location, this, 2, whiteRectangle, Color.Red);
+            }
+            foreach (Bullet bullet in bullets)
+            {
+                if (bullet != null)
+                {
+                    bullet.Update();
+                }
+            }
+            if(tank1Explosion != null)
+            {
+                tank1Explosion.Update();
+            }
+            if(tank2Explosion != null)
+            {
+                tank2Explosion.Update();
+            }
             base.Update(gameTime);
         }
 
@@ -90,6 +125,21 @@ namespace Tank_Game
             spriteBatch.Begin();
             tank1.Draw(spriteBatch);
             tank2.Draw(spriteBatch);
+            foreach(Bullet bullet in bullets)
+            {
+                if (bullet != null)
+                {
+                    bullet.Draw(spriteBatch);
+                }
+            }
+            if (tank1Explosion != null)
+            {
+                tank1Explosion.Draw(spriteBatch);
+            }
+            if (tank2Explosion != null)
+            {
+                tank2Explosion.Draw(spriteBatch);
+            }
             spriteBatch.End();
 
             base.Draw(gameTime);
